07967d403ad4ba602a10e10a9617a943daf4b65a
EomYoosang <dbtkd1801@gmail.com>
2022-02-08 15:35:10 +0900

    bug: 연관관계 매핑 수정

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.post.hasMany(models.Post);
+      models.Post.hasMany(models.Post);
     };
   };
   User.init({
