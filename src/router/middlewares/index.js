0acead732c1d67740e6412d0d77ed0d0b00df453
EomYoosang <dbtkd1801@gmail.com>
2022-02-08 15:36:56 +0900

    feat: router middleware 추가

---
+++ src/router/middlewares/index.js
@@ -0,0 +1,5 @@
+import responseMd from "./responseMd";
+import generateJwtMd from "./generateJwtMd";
+import getTokenMd from "./getTokenMd";
+
+export { responseMd, generateJwtMd, getTokenMd };
---
+++ src/router/user/index.js
@@ -0,0 +1,10 @@
+import Router from "@koa/router";
+import * as user from "./user";
+
+const router = new Router();
+
+// router.post("/", ...user.create);
+
+// router.post("/login", ...user.login);
+
+export default router;
